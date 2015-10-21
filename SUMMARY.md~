1df6289669c7149527c4c918339fd23c64cc2d64
Wills Wong <wills.wong49@gmail.com>
2015-10-21 23:53:33 +0800

    _src/om2py0w/0wex1/README.md

--- SUMMARY.md~
+++ SUMMARY.md~
@@ -1,8 +1,8 @@
 # Summary
 - [学习记录](0MOOC/README.md)
  + [Week 0](0MOOC/Week0/README.md)
-  + [Git与Github的配置过程](0MOOC/Week0/Git and Github config.md)
-  + [0wd4公开课纪要](0MOOC/Week0/0wd4 Note.md)
+  * [Git与Github的配置过程](0MOOC/Week0/Git and Github config.md)
+  * [0wd4公开课纪要](0MOOC/Week0/0wd4 Note.md)
 - [基础旋进](1sTry/README.md)
 - [迭代作品](2nDev/README.md)
 - [人生首秀](3rDemo/README.md)
--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -5,27 +5,27 @@ _author_ = 'Wills Wong'
 
 import os
 
-def input_one_line(count):
-    line_num = 'Line %d :' % count
-    a = raw_input(line_num) + '\n'
-    with open('/home/wong/test.txt','a') as fileName:
-        fileName.write(a)
-
 if os.path.exists('/home/wong/test.txt') == True:
     print '\n*** Here is your writting record ***\n\n'
     with open('/home/wong/test.txt','r') as f:
         print f.read()
         
 print '\nNow you can start writing:\n'
-
-print'PS: When you complete writing, please hit Ctrl - C !\n'
+print 'PS: When you complete writing, please hit "Q" or "q" !\n'
 
 line = 0
 
 while True:
-	try:
-		input_one_line(line)
-		line += 1
-	except KeyboardInterrupt:
-		print '\n\nThanks for using, goobye! ^_^\n'
-		break
+    try:
+        line_num = 'Line %d :' % line
+        a = raw_input(line_num) + '\n'
+        if a == 'Q\n' or a == 'q\n':		
+            print '\n\nThanks for using. Goodbye! ^_^\n'
+            break
+        with open('/home/wong/test.txt','a') as f:
+            f.write(a)
+        line += 1
+    except KeyboardInterrupt:
+        print '\n\nError! You can not hit "Ctrl-C" when you writing, it will interupt input and the current line will lose!\n'
+        break
+
