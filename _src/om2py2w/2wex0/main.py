80629f2d3d597f6a65a02bcc3a1a97925d26644a
Wills Wong <wills.wong49@gmail.com>
2015-10-29 00:38:53 +0800

    _src/om2py2w/2wex0/main.py

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -2,56 +2,75 @@
 #!/usr/bin/env python
 
 """
-------------------------Version 1.0-------------------------
-此脚本是桌面版极简交互式日记系统的原始版本，能实现基本功能。用户
-在运行该脚本后，能窗口中输入日记内容，并保存为本地文件。同时，对
-于非首次使用用户，脚本运行后还能在窗口中输出所有历史日记内容。
+------------------------Version 2.0-------------------------
+该版本在1.0的基础上，增加了功能按键，通过不同的功能按键实现相应
+的功能
 """
 
 #全局引用
 from Tkinter import *
-import os,sys
+import os,sys,time,tkMessageBox
 
 def main():
     
     #全局变量
     local = ''
+    filePath = '/home/wong/test.txt'
 
     #加载sys后，setdefaultencoding方法会被删除，所以需要重新加载
     #（reload）sys才能调用setdefaultencoding方法，重新设置系统编码
     reload(sys)
     sys.setdefaultencoding( "utf-8" )
 
-    #初始化Tk，并设置窗口标题为“My Diary”
+    #初始化Tk，并设置窗口标题为“我的日记本”
     root = Tk()
-    root.title('My Diary')
+    root.title('我的日记本')
 
-    #定义按键“Save”的事件处理函数：点击按键后保存所输入的全部内容
-    def saveText():
-        with open('/home/wong/test.txt','a') as f:
-            f.write(test.get(local,END))
-
-    #创建一个Text控件和Button控件
+    #创建Text控件
     test = Text(root)
-    buttons = Button(root, text = 'Save', command = saveText)
-
+    
     #创建两个关于颜色的文本属性tag，红色用于系统提示语，蓝色用于历史日志输出
     test.tag_config('red',foreground = 'red')
     test.tag_config('blue',foreground = 'blue')
 
-    #首次输入检测，否则打印历史日志
-    if os.path.exists('/home/wong/test.txt') == True:
-        test.insert(1.0,'*** Here is your writting record ***\n','red')
-        with open('/home/wong/test.txt','r') as f:
-            test.insert(CURRENT, f.read() + '\n', 'blue')
+    #定义按键“退出”的事件处理函数
+    def quit():
+        with open(filePath,'a') as f:
+            f.write('\n' + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + '\n' + test.get(local,END))
+        root.destroy()
 
-    test.insert(CURRENT, 'Now you can start writing:', 'red')
+    #定义按键“日记历史”的时间处理函数
+    def history():
+        if os.path.exists(filePath) == True:
+            with open(filePath,'r') as f:
+                testshow = '*** Hi 这是您以前的日记 ***\n' + f.read()
+            tl = Toplevel()
+            tl.title('您的日记历史')
+            test_tl = Text(tl)
+            test_tl.insert(1.0,testshow)
+            test_tl.pack()
+        else:
+            tkMessageBox.showinfo('Error','Sorry，您还没有写过日记呢！赶紧行动起来吧！')
+
+    #定义按键“新日记”的事件处理函数
+    def new():
+        test.delete(1.0,END)
+        test.insert(CURRENT, '现在您可以开始写新日记啦', 'red')
+    
+    #定义按键“关于”的事件处理函数
+    def about():
+        tkMessageBox.showinfo('关于该软件的信息','Author: Wills Wong\nVersion: 2.0')
+    
+    #创建Button控件
+    Button(root, text = '退出', relief = GROOVE, fg = 'red', command = quit).pack(fill=X)
+    Button(root, text = '日记历史', relief = GROOVE, fg = 'red', command = history).pack(fill=X)
+    Button(root, text = '新日记', relief = GROOVE, fg = 'red', command = new).pack(fill=X)
+    Button(root, text = '关于', relief = GROOVE, fg = 'red', command = about).pack(fill=X)
 
     #获取新内容的起始光标位置
     local = test.index(CURRENT)
 
-    #将buttons和test加入到root并布局，最后进入消息循环
-    buttons.pack()
+    #将test加入到布局，最后进入消息循环
     test.pack()
     root.mainloop()
 
